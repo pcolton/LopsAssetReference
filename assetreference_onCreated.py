61d31ae91f040e81e0bc482ebda0cbe223e3e1ef
Paul Colton <pcolton@users.noreply.github.com>
2024-04-29 12:20:34 -0400

    add prim name and default prim type
    - added primitive name with default value
    - set Parent Primitive Type to Scope

--- assetreference_onCreated.py
+++ assetreference_onCreated.py
@@ -1,13 +1,14 @@
-# Created by Paul Colton for Rebekway Cinematic Lighting in Houdini class
+# Created by Paul Colton for Rebelway Cinematic Lighting in Houdini class
 # Based on script from https://www.toadstorm.com/blog/?p=1012
-# v1.00
+# v1.0.1
 
 import hou
 import traceback
 import time
 import threading
 
-destinationPrimitivePath = '`chs("category")``chs("subcategory")`/$OS'
+primitiveName = "/$OS"
+destinationPrimitivePath = '`chs("category")``chs("subcategory")``chs("primname")`'
 
 categories = [
     "/env",
@@ -33,6 +34,7 @@ me = kwargs['node']
 
 def delayedSet():
     time.sleep(0.01)
+    me.parm("parentprimtype").set("UsdGeomScope")
     me.parm("primpath").set(destinationPrimitivePath)
 
 try:
@@ -41,21 +43,24 @@ try:
     parm_group = me.parmTemplateGroup()
  
     # we'll put this new spare parm right before the "Destination Primitive" parameter, named "primpath".
-    range_template = parm_group.find("primpath")
+    primpath_template = parm_group.find("primpath")
 
      # Code for parameter template
     hou_parm_template = hou.MenuParmTemplate("category", "Category", menu_items=(categories), menu_labels=(categories))
     hou_parm_template.setJoinWithNext(True)
-    parm_group.insertBefore(range_template, hou_parm_template)
+    parm_group.insertBefore(primpath_template, hou_parm_template)
 
     # Code for parameter template
     hou_parm_template = hou.MenuParmTemplate("subcategory", "Sub-Category", menu_items=([""] + subCategories), menu_labels=(["None"] + subCategories))
-    parm_group.insertBefore(range_template, hou_parm_template)
- 
+    parm_group.insertBefore(primpath_template, hou_parm_template)
+
+    hou_parm_template = hou.StringParmTemplate(name="primname", label="Primitive Name", num_components=1, default_value=([primitiveName]))
+    parm_group.insertBefore(primpath_template, hou_parm_template)
+
     # now we need to write this modified ParmTemplateGroup back to the individual node's ParmTemplateGroup.
     # this is effectively how you add a spare parm to a node without modifying the HDA itself.
     me.setParmTemplateGroup(parm_group)
- 
+
     threading.Thread(target=delayedSet).start()
 
 except Exception as err:
